38b95642e45268f8bf9c2c27b5de366681640075
Cholewa98 <cholewka.michal@gmail.com>
2020-05-13 13:45:03 +0200

    add functionality to product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,19 +5,22 @@ import {
   AsyncStorage,
   FlatList,
   TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
+import {List, Checkbox} from 'react-native-paper';
 
 export default class ProductList extends Component {
   id = {basketId: this.props.route.params.user};
-
+  productId = '';
   constructor(props) {
     super(props);
     this.state = {
       data: [],
       isLoading: true,
+      checked: false,
+      checkboxes: [],
     };
   }
-
   componentWillMount = async () => {
     const {basketId} = this.id;
     console.log(basketId);
@@ -43,9 +46,46 @@ export default class ProductList extends Component {
       });
   };
 
+  setChecked = async isBool => {
+    console.log('elo elo ');
+    const {basketId} = this.id;
+    const {productId} = this.productId;
+    console.log(isBool);
+    var token = await AsyncStorage.getItem('token');
+    console.log(this.productId);
+    const url =
+      'http://192.168.0.105:3000/api/buckets/' +
+      basketId +
+      '/products/' +
+      this.productId;
+      console.log(isBool);
+    fetch(url, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'auth-token': token,
+      },
+      body: JSON.stringify({
+        isBought: !isBool,
+      }),
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({data: json.products});
+        console.log(json);
+        this.componentWillMount();
+      })
+      .catch(error => console.error(error))
+      .finally(() => {
+        this.setState({isLoading: false});
+      });
+  };
+
   render() {
     const {data} = this.state;
     const {basketId} = this.id;
+    const {checked} = this.state;
     return (
       <View>
         <Text>ELO JESTES TU</Text>
@@ -54,15 +94,49 @@ export default class ProductList extends Component {
           data={data}
           keyExtractor={({_id}, index) => _id}
           renderItem={({item}) => (
-            <TouchableOpacity
-              onPress={() => {
-                this.handleNavigate(item._id);
-              }}>
-              <Text>{item.productName}</Text>
-            </TouchableOpacity>
+            <View style={styles.listContainer}>
+              <List.Section style={styles.list}>
+                <List.Accordion
+                  title={item.productName}
+                  left={props => <List.Icon {...props} icon="folder" />}>
+                  <List.Item title="Quantity" description={item.quantity} />
+                  <List.Item
+                    title="Description"
+                    description={item.description}
+                  />
+                </List.Accordion>
+              </List.Section>
+              <Checkbox
+                style={styles.checkbox}
+                status={item.isBought ? 'checked' : 'unchecked'}
+                onPress={() => {
+                  this.productId = item._id;
+                  console.log(this.productId);
+                  console.log('wszedłem');
+                  console.log(item.isBought);
+                  this.setChecked(item.isBought);
+                  //item.isBought = true;
+                }}
+              />
+            </View>
           )}
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  listContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  list: {
+    width: '87%',
+  },
+  checkbox: {
+    height: 120,
+  },
+});
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,111 +1,3 @@
-// import React, {Component} from 'react';
-// import {
-//   View,
-//   StyleSheet,
-//   Text,
-//   TextInput,
-//   TouchableOpacity,
-// } from 'react-native';
-
-// export default class Register extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       email: '',
-//       displayName: '',
-//       password: '',
-//       confirmPassword: '',
-//     };
-//   }
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.logo}> Register </Text>
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Email"
-//           onChangeText={email => this.setState({email})}
-//           value={this.state.email}
-//         />
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Name"
-//           onChangeText={displayName => this.setState({displayName})}
-//           value={this.state.displayName}
-//         />
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Password"
-//           secureTextEntry
-//           onChangeText={password => this.setState({password})}
-//           value={this.state.password}
-//         />
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Confirm password"
-//           secureTextEntry
-//           onChangeText={confirmPassword => this.setState({confirmPassword})}
-//           value={this.state.confirmPassword}
-//         />
-//         <TouchableOpacity style={styles.userBtn} onPress={this._register}>
-//           <Text>Register</Text>
-//         </TouchableOpacity>
-//       </View>
-//     );
-//   }
-
-//   _register = () => {
-//     fetch('http://192.168.0.105:3000/api/users/register', {
-//       method: 'POST',
-//       headers: {
-//         Accept: 'application/json',
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({
-//         email: this.state.email,
-//         displayName: this.state.displayName,
-//         password: this.state.password,
-//       }),
-//     });
-//   };
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#4287f5',
-//   },
-//   logo: {
-//     fontSize: 50,
-//     paddingBottom: 30,
-//     color: '#ffffff',
-//   },
-//   input: {
-//     width: '90%',
-//     backgroundColor: '#ffffff',
-//     borderRadius: 5,
-//     marginTop: 10,
-//   },
-//   userBtn: {
-//     backgroundColor: '#FFD700',
-//     alignItems: 'center',
-//     height: 50,
-//     justifyContent: 'center',
-//     borderRadius: 8,
-//     marginTop: 20,
-//     width: '80%',
-//   },
-//   btnContainer: {
-//     fontSize: 18,
-//     width: '90%',
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     paddingTop: 20,
-//   },
-// });
-
 import React, {Component} from 'react';
 import {
   StyleSheet,
@@ -114,6 +6,7 @@ import {
   TextInput,
   TouchableOpacity,
 } from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {Icon} from 'react-native-elements';
 import {AuthContext} from '../../context';
 
